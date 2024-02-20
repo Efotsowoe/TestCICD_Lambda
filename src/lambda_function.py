446771a89b2d58202c8b8d7b259e632be922186d
Mpcsj <marcosjunioribm@gmail.com>
2024-02-19 23:31:10 -0300

    fix: Fix import statement :bug:

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,5 +1,5 @@
 import json
-from src.extra_utils import get_lambda_message
+from extra_utils import get_lambda_message
 
 
 def lambda_handler(event, context):
@@ -8,3 +8,7 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': json.dumps(get_lambda_message())
     }
+
+
+if __name__ == "__main__":
+    print(lambda_handler({}, {}))
