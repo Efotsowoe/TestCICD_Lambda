c205ac7ba8da19737f91104f6d8919d9c8c451a4
Mpcsj <marcosjunioribm@gmail.com>
2024-02-20 20:30:06 -0300

    test: Add tests to the codebase :white_check_mark: :heavy_check_mark:

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -8,3 +8,7 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': json.dumps(get_lambda_message())
     }
+
+
+if __name__ == "__main__":
+    print(lambda_handler({}, {}))
