69ea2adbff06d27370bdbef1a5ea6a4aa7938ed5
Mpcsj <marcosjunioribm@gmail.com>
2024-02-19 23:28:28 -0300

    feat: Create second module :sparkles:

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,9 +1,10 @@
 import json
+from src.extra_utils import get_lambda_message
 
 
 def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda! - Updated V2')
+        'body': json.dumps(get_lambda_message())
     }
